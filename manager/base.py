d31637b17328c67a5494d9c9e8fc9efa0de71590
XYCode Kerman <xycode-xyc@outlook.com>
2024-03-26 12:52:24 +0800

    新增：配置文件及其Pydantic类

--- manager/base.py
+++ manager/base.py
@@ -9,6 +9,15 @@ from utils import manager_logger
 from .api import app as api
 
 
+def init_config():
+    configdir = pathlib.Path('./config')
+
+    configdir.mkdir(exist_ok=True, parents=True)
+    if not configdir.exists():
+        configdir.joinpath('contests.json').touch(exist_ok=True)
+        configdir.joinpath('contests.json').write_text('[]', encoding='utf-8')
+
+
 def _start_server():  # pragma: no cover
     uvicorn.run(api, host="0.0.0.0", port=2568)
 
@@ -18,3 +27,6 @@ def start_server_background():  # pragma: no cover
         target=_start_server)
     server_thread.start()
     return server_thread
+
+
+init_config()
