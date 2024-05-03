5b00e7b0fbbecc71856be3db5bc087c0a6c758f3
XYCode Kerman <xycode-xyc@outlook.com>
2024-05-03 18:48:15 +0800

    refactor: 新增docs分支

--- configs.py
+++
@@ -1,3 +0,0 @@
-# 配置LRUN用户和组（不要将其设置为有root权限的用户！）
-LRUN_UID = 1001
-LRUN_GID = 1001
--- online_judge/oj_models/user.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class User(BaseModel):
-    username: str
-    password: str
-    role: str
