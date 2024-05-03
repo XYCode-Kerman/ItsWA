c06c7f68b5461bdacc965c6c4b642317deed9e5a
XYCode Kerman <xycode-xyc@outlook.com>
2024-05-03 20:04:21 +0800

    test: 新增使用File-IN/OUT时未指定file_input_path时的情况
    Module: judge.runtime

--- tests/test_judge/test_runtime.py
+++ tests/test_judge/test_runtime.py
@@ -12,12 +12,14 @@ from ccf_parser.status import Status
 from judge import ReportAnalyze, simple_judging, start_judging
 from judge.languages import CPP, Language
 from judge.runtime import SafetyRuntime, SimpleRuntime, runtime
+from utils.logger import judge_logger
 
 simple_runtime = SimpleRuntime()
 # Root
 if os.getuid() == 0 and os.getgid() == 0:
     safety_runtime = SafetyRuntime()
 else:
+    judge_logger.warning('未使用root用户运行测试，将安全运行时回退到简单运行时')
     safety_runtime = SimpleRuntime()
 
 
@@ -43,6 +45,15 @@ def test_simple_runtime():
             input_type,
             pathlib.Path('a.in')
         )[0]) in [Status, str]
+
+    with pytest.raises(ValueError):
+        # 文件输入输出不存在时
+        simple_runtime(
+            pathlib.Path('./tests/environment/executeables_to_test/ac'),
+            '',
+            input_type='FILE',
+            file_input_path=None
+        )
 
     assert simple_runtime(
         pathlib.Path('./tests/environment/executeables_to_test/ac'),
@@ -74,6 +85,15 @@ def test_safety_runtime():
             input_type,
             pathlib.Path('a.in')
         )[0]) in [Status, str]
+
+    with pytest.raises(ValueError):
+        # 文件输入输出不存在时
+        safety_runtime(
+            pathlib.Path('./tests/environment/executeables_to_test/ac'),
+            '',
+            input_type='FILE',
+            file_input_path=None
+        )
 
     assert safety_runtime(
         pathlib.Path('./tests/environment/executeables_to_test/ac'),
